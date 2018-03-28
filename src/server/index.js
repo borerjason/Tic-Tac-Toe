868f56bf9af1cb12fb79b4b6c23fd75b277e4ca6
Jason Borer <borerjason@gmail.com>
2018-03-27 17:10:46 -0700

    socket.io require

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const path = require('path');
 
 const app = express();
 
 app.use( '/', express.static(path.join(__dirname, '../../dist')));
+
+io.on('connection', (socket) => {
+  console.log('A player connected');
+});
 
 const PORT = process.env.PORT || 3000;
 
